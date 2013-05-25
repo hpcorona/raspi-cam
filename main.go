57537c292fefba8d2671b0000c5ad6eb1b9af230
Hilario Perez Corona <hpcorona@gmail.com>
2013-05-25 13:19:02 -0700

    Create window from start

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	log.Printf("Running!")
 
+	win := cv.NewWindow("Image")
+	defer win.Destroy()
+
 	//capture := cv.NewCaptureFromCAM(cv.CV_CAP_ANY)
 	capture := cv.NewCameraCapture(cv.CV_CAP_ANY)
 	capture.SetProperty(cv.CV_CAP_PROP_FRAME_WIDTH, 320)
@@ -18,8 +21,6 @@ func main() {
 
 	image := capture.QueryFrame()
 	if image != nil {
-		win := cv.NewWindow("Image")
-		defer win.Destroy()
 		win.ShowImage(image)
 		//win.Resize(640, 480)
 		cv.WaitKey(0)
