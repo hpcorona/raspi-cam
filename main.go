bd82a19dc3ce34fe2151332677299c037bf31f65
Hilario Perez Corona <hpcorona@gmail.com>
2013-05-25 12:38:11 -0700

    Cant save :(… Maybe show?

--- main.go
+++ main.go
@@ -9,14 +9,19 @@ func main() {
 	log.Printf("Running!")
 
 	capture := opencv.NewCameraCapture(opencv.CV_CAP_ANY)
+	defer capture.Release()
 
 	image := capture.QueryFrame()
 	if image != nil {
-		opencv.SaveImage("test.png", image, 0)
+		win := opencv.NewWindow("Image")
+		defer win.Destroy()
+		win.ShowImage(image)
+		//win.Resize(640, 480)
+		opencv.WaitKey(0)
+
+		//opencv.SaveImage("test.png", image, 0)
 		log.Printf("WE HAVE IMAGE :)")
 	} else {
 		log.Printf("NO IMAGE :(")
 	}
-
-	capture.Release()
 }
