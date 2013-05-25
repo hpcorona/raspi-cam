70301e949b5c4f7458508c2491a38b386d1753a1
Hilario Perez Corona <hpcorona@gmail.com>
2013-05-25 11:29:55 -0700

    Test 1

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"code.google.com/p/go-opencv/opencv"
 	"log"
 )
 
 func main() {
 	log.Printf("Running!")
+
+	capture := opencv.NewFileCapture("/dev/video0")
+
+	image := capture.QueryFrame()
+	if image != nil {
+		SaveImage("test.jpg", image, 0)
+		log.Printf("WE HAVE IMAGE :)")
+	} else {
+		log.Printf("NO IMAGE :(")
+	}
+
+	image.Release()
+
+	capture.Release()
 }
